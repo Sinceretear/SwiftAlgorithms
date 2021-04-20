1dd858444ea47c1c30da01736991f909b632c6ee
Hunter <bobnamed123@yahoo.com>
2021-04-20 14:34:22 -0700

    Add files via upload

--- LinkedListx2.playground/Contents.swift
+++ LinkedListx2.playground/Contents.swift
@@ -128,6 +128,73 @@ public struct LinkedList<Value> {
     }
     
     
+    @discardableResult
+    public mutating func pop() -> Value? {
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        
+        guard let head = head else {
+            return nil
+        }
+        guard head.next != nil else {
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+    
+    //call this on top of every mutating method to allow C-O-W
+    //this is to implement the copy-on-write functionality, currently nodes are reference types and not value types
+    //book says callying this is O(n) overhead
+    // finish this on page 82
+    private mutating func copyNodes() {
+        guard var oldNode = head else {
+            return
+        }
+        
+        head = Node(value: oldNode.value)
+        var newNode = head
+        
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            oldNode = nextOldNode
+        }
+        tail = newNode
+    }
+    
+
     
 }
 
@@ -141,9 +208,76 @@ extension LinkedList: CustomStringConvertible {
 }
 
 
+
 var list = LinkedList<Int>()
 list.append(1)
 list.append(2)
 list.append(3)
+list.append(4)
+
+list.pop()
+list.removeLast()
+
+list.append(5)
+let node = list.node(at: 1)!
+list.remove(after: node)
 
 print(list)
+
+
+
+extension LinkedList: Collection {
+    
+    public struct Index: Comparable {
+        
+        public var node: Node<Value>?
+        
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) {$0?.next}
+            return nodes.contains {$0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
+
+var listColl = LinkedList<Int>()
+
+for i in 0...9 {
+    listColl.append(i)
+}
+
+print(listColl)
+print(listColl[listColl.startIndex])
+print(Array(listColl.prefix(3)))
+print(Array(listColl.suffix(3)))
+
+let sum = listColl.reduce(0, +)
+print(sum)
